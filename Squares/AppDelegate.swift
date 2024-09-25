713c9b611f45420ba75288a196ca4f8af06e5596
Tadevos Kurdoglian <tazman311@gmail.com>
2024-09-25 13:27:17 +0300

    Initial Commit

---
+++ Squares/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Squares
+//
+//  Created by Тадевос Курдоглян on 25.09.2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Squares/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Squares
+//
+//  Created by Тадевос Курдоглян on 25.09.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
